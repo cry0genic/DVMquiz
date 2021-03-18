049b255a64a5256bd45697ffb217cddbdb84a6d8
cry0genic <cry0genic1103@gmail.com>
2021-03-18 19:10:51 +0530

    b

--- gamblingMaths/admin.py
+++ gamblingMaths/admin.py
@@ -2,18 +2,18 @@ from django.contrib import admin
 from .models import Member, Question, Answer, Response, MemberQuestion
 from django.contrib.auth.models import Group
 
-# class AnswerInline(admin.StackedInline):
-#     model = Answer
-#     extra = 4
+class AnswerInline(admin.StackedInline):
+    model = Answer
+    extra = 4
 
-# @admin.register(Question)
-# class QuestionAdmin(admin.ModelAdmin):
-#     fields = ['questionkey', 'content', 'pool', 'image', ('is_image', 'is_mcq'), 'answer']
-#     inlines = [AnswerInline]
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['questionkey', 'content', 'pool', 'image', ('is_image', 'is_mcq'), 'answer']
+    inlines = [AnswerInline]
 
 admin.autodiscover()
 admin.site.register(Member)
-admin.site.register(Question)
+#admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Response)
 admin.site.register(MemberQuestion)
